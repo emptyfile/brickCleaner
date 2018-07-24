b389a4c3d840d075d73c04f689c0608ab811fc86
Ursatii <zabybenskiy@gmail.com>
2018-07-24 23:50:11 +0300

    Made user name a constant.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -7,16 +7,16 @@ import java.io.File;
 import java.util.Collection;
 
 public class Controller {
+    private static final String USER = System.getProperty("user.name");
     @FXML
     public CheckBox chromeCacheCB;
     @FXML
     public Button cleanButton;
     @FXML
     void initialize(){
-        String user = System.getProperty("user.name");
         cleanButton.setOnAction((e)->{
             if(chromeCacheCB.isSelected()){
-                File chromeCache = new File("C:\\Users\\" + user + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cache");
+                File chromeCache = new File("C:\\Users\\" + USER + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cache");
                 Collection<File> fileCollection = FileUtils.listFiles(chromeCache, null, true);
                 fileCollection.forEach(a->a.delete());
             }
