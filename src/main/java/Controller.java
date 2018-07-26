e9792f8a62e6e3cea6a7609129bee7276844d03d
Ursatii <zabybenskiy@gmail.com>
2018-07-26 13:33:00 +0300

    Added Google Chrome, Chrome Cookies and Chrome History CheckBoxes and functionality for them.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,13 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.UUID;
 
 public class Controller {
     private static final String USER = System.getProperty("user.name");
@@ -13,13 +16,60 @@ public class Controller {
     @FXML
     public Button cleanButton;
     @FXML
+    public CheckBox chromeHistoryCB;
+    @FXML
+    public CheckBox chromeCookiesCB;
+    @FXML
+    public CheckBox googleChromeCB;
+
+    @FXML
     void initialize(){
+        chromeCookiesCB.setDisable(true);
+        chromeHistoryCB.setDisable(true);
+        chromeCacheCB.setDisable(true);
         cleanButton.setOnAction((e)->{
+
             if(chromeCacheCB.isSelected()){
                 File chromeCache = new File("C:\\Users\\" + USER + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cache");
                 Collection<File> fileCollection = FileUtils.listFiles(chromeCache, null, true);
                 fileCollection.forEach(a->a.delete());
             }
+
+            if (chromeHistoryCB.isSelected()){
+                File chromeHistory = new File("C:\\Users\\"+ USER+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History");
+                try {
+                    FileUtils.forceDelete(chromeHistory);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            if (chromeCookiesCB.isSelected()){
+                File chromeCookies = new File("C:\\Users\\"+ USER+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies");
+                try {
+                    FileUtils.forceDelete(chromeCookies);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
         });
+    }
+
+    @FXML
+    public void onClickMethodChrome(MouseEvent mouseEvent) {
+        if (googleChromeCB.isSelected()){
+        chromeCookiesCB.setDisable(false);
+        chromeHistoryCB.setDisable(false);
+        chromeCacheCB.setDisable(false);
+        chromeCacheCB.setSelected(true);
+        }else {
+            chromeCookiesCB.setDisable(true);
+            chromeHistoryCB.setDisable(true);
+            chromeCacheCB.setDisable(true);
+            chromeCacheCB.setSelected(false);
+            chromeCookiesCB.setSelected(false);
+            chromeHistoryCB.setSelected(false);
+        }
+
     }
 }
